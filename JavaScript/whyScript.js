2c07dca3e086007d907f6d87c1e8cee614190e3c
lokesh <lokesh1129ece@gmail.com>
2023-02-22 19:23:59 +0530

    Optimized JS code

--- JavaScript/whyScript.js
+++ JavaScript/whyScript.js
@@ -1,72 +1,53 @@
 
-let arrowPointing = document.querySelectorAll('.arrow-pointing');
 
-let reasonImg = document.querySelectorAll('.reason-img');
+const arrowPointing = document.querySelectorAll('.arrow-pointing');
+const reasonImg = document.querySelectorAll('.reason-img');
+const heroSection = document.querySelector('.hero');
+const headerSection = document.querySelector('.header');
+const mainSection = document.querySelector('main');
+const goUpIcon = document.querySelector('.go-up-icon');
 
-let heroSection = document.querySelector('.hero');
-
-let headerSection = document.querySelector('.header');
-
-let mainSection = document.querySelector('main');
-
-let goUpIcon = document.querySelector('.go-up-icon');
-
-
-
-
-
-
-
-const showArrow = function(entries,observer){
-const [entry] = [...entries];
-
-entry.target.classList.toggle('set-opacity-visible',entry.isIntersecting);
-
-if(entry.target.classList.contains('reason-img')){
-  entry.target.classList.toggle('shadow-effect',entry.isIntersecting);
-}
-
-}
-
-const arrowObserver = new IntersectionObserver(showArrow,{
-  root:null,
-  threshold:1,
-  rootMargin:'-100px'
-});
-
-arrowPointing.forEach((arrow)=>arrowObserver.observe(arrow));
-
-reasonImg.forEach((img)=>arrowObserver.observe(img));
-
-
-// Implementing Sticky Navigation
-
-const stickNav = function(entries,observer){
-  const [entry] = [...entries];
-  console.log(entry);
-  
-  if(!entry.isIntersecting){
-    console.log('1');
-headerSection.classList.add('sticky');
-mainSection.style.marginTop="9.6rem";
-goUpIcon.style.opacity="1";
-goUpIcon.style.transform="translateY(25rem)";
-
-  }else{
-    headerSection.classList.remove('sticky');
-    mainSection.style.marginTop="0rem";
-    goUpIcon.style.transform="translateY(0)";
-    goUpIcon.style.opacity="0";
+const shadowEffect = function (entry) {
+ 
+  if (entry.target.classList.contains('reason-img')) {
+    entry.target.classList.toggle('shadow-effect', entry.isIntersecting);
   }
-
-
 };
 
-const stickyNavObserver = new IntersectionObserver(stickNav,{
-root:null,
-threshold:0,
+const imgObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    shadowEffect(entry);
+  });
+}, {
+  root: null,
+  threshold: 1,
+  rootMargin: '-10px',
+});
 
+
+reasonImg.forEach((img) => imgObserver.observe(img));
+
+const stickNav = function (entry) {
+  if (!entry.isIntersecting) {
+    headerSection.classList.add('sticky');
+    mainSection.style.marginTop = '9.6rem';
+    goUpIcon.style.opacity = '1';
+    goUpIcon.style.transform = 'translateY(25rem)';
+  } else {
+    headerSection.classList.remove('sticky');
+    mainSection.style.marginTop = '0rem';
+    goUpIcon.style.transform = 'translateY(0)';
+    goUpIcon.style.opacity = '0';
+  }
+};
+
+const stickyNavObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    stickNav(entry);
+  });
+}, {
+  root: null,
+  threshold: 0,
 });
 
 stickyNavObserver.observe(heroSection);
-
